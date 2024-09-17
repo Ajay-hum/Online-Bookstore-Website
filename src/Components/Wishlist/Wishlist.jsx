1406f2265ad61c8ecd5bfdeef81dfaa7ced0dcc4
Ajay-hum <justuschimeremueze@gmail.com>
2024-09-17 10:18:17 +0100

    update

--- src/Components/Wishlist/Wishlist.jsx
+++ src/Components/Wishlist/Wishlist.jsx
@@ -1,8 +1,8 @@
-const addToWishlist = (post) => {
-  const wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
-  wishlist.push(post);
-  localStorage.setItem('wishlist', JSON.stringify(wishlist));
-};
+// const addToWishlist = (post) => {
+//   const wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+//   wishlist.push(post);
+//   localStorage.setItem('wishlist', JSON.stringify(wishlist));
+// };
 
-// In the other components, I will add a button to save books to the wishlist
-<button onClick={() => addToWishlist(book)}>Add to Wishlist</button>
+// // In the other components, I will add a button to save books to the wishlist
+// <button onClick={() => addToWishlist(book)}>Add to Wishlist</button>
